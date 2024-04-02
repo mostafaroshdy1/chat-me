3ca4b3afd5efe45e80aa74ec0b80521511b467f3
Mostafa Elgreatly <66712535+mostafaroshdy1@users.noreply.github.com>
2024-04-02 12:33:56 +0200

    feat: online chat room

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import { createServer } from 'node:http';
+import { Server } from "socket.io";
+import ejs from 'ejs';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const server = createServer(app);
+const io = new Server(server);
+
+
+// app.use(express.json()) // for parsing application/json
+// app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+// app.use(express.static('public'));
+app.engine('html', ejs.renderFile);
+app.set('view engine', ejs);
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.render('index.html');
+});
+
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log('server running at http://localhost:3000');
+})
